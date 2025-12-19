dfc8cf83ce8e8c7f3c2e2ee38e332acd075c56b5
Omar Semgey <omar.semgey@gmail.com>
2025-12-19 15:43:37 +0300

    Fixing Bugs

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,20 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'paths' => ['api/*', 'auth/*', 'counts/*', 'client-logs'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'], 
+
+    'allowed_origins' => ['https://lamatics.onrender.com'], 
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => true,
+
+    'supports_credentials' => true, 
 ];
