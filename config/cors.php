fabf56bf53cfc0ae2c5842c4a7a3631332819730
Omar Semgey <omar.semgey@gmail.com>
2025-12-22 19:26:01 +0300

    Fixing Bugs

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://lamatics.onrender.com', "http://localhost:5173"], 
+    'allowed_origins' => ['https://lamatics.onrender.com'], 
 
     'allowed_origins_patterns' => [],
 
